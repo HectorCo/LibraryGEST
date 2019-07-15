0ee7648dd19603d8f2da503e79ffd1d6e7bd7d27
HectorCo <hcompanyortega@gmail.com>
2019-07-15 22:22:49 +0200

    finalizado funcionamiento pantalla detalle libro, faltan metodos

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LibrosService } from '../../shared/services/libros.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,13 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
+  public id: any;
+  public libro: any = {};
 
-  constructor() { }
+
+  constructor(
+    public api: LibrosService,
+    public route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.id = params.id;
+      this.libro = this.api.libroSeleccion(this.id);
+  });
+
+
+
   }
 
   borrarLibro(){}
 
   editar(){}
 }
+
+
+
+
