0b192747f4fd99d8d94bdd3e44cec2889b7b5ba5
HectorCo <hcompanyortega@gmail.com>
2019-07-16 12:43:25 +0200

    Barra de búsqueda/filtrado libro terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { CommonsModule } from './commons/commons.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsModule } from './products/products.module';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
-  
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CommonsModule,
+    Ng2SearchPipeModule,
+    FormsModule,
     ProductsModule
   ],
   providers: [],
